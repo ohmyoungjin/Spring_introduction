26483c0230eda50a03e00c0b16007159e4d8ba0c
ohmyoungjin <sisaudwls@naver.com>
2022-10-14 14:23:59 +0900

    20221014 :: MemverForm & edit MemberController add

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,13 +1,16 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
 
-    private final MemberService memberService ;
+    private final MemberService memberService;
 
     @Autowired
     public MemberController(MemberService memberService) {
@@ -15,5 +18,19 @@ public class MemberController {
     }
 
 
+    @GetMapping("/members/new")
+    public String createForm() {
+        System.out.println("넘어온다~");
+        return "members/createMemberForm";
+    }
 
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 }
