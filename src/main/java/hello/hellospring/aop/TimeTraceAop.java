79ddc35ae9599bf9d4238d282d58b3d7646140d8
ohmyoungjin <sisaudwls@naver.com>
2022-10-16 22:30:39 +0900

    20221016 :: AOP LOGIC add && spring_introduction study final ! good job

---
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,21 @@
+package hello.hellospring.aop;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+@Component
+@Aspect
+public class TimeTraceAop {
+    @Around("execution(* hello.hellospring..*(..))") //이 부분이 어느 패키지 어느 파일에 적용할지 나타내는 부분
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString()+ " " + timeMs + "ms");
+        }
+    }
+}
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -17,26 +17,18 @@ public class MemberService {
 
 
     public MemberService(MemberRepository memberRepository) {
+
         this.memberRepository = memberRepository;
+        System.out.println("memberService : " + memberRepository.getClass());
     }
     /**
      * 회원 가입
      */
     public Long join(Member member) {
         //같은 이름있는 중복 회원 X
-        long start = System.currentTimeMillis();
-
-        try{
             validateDuplicateMember(member);
             memberRepository.save(member);
             return member.getId();
-        }finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("timeMs = " + timeMs + "ms");
-
-        }
-
     }
 
     private void validateDuplicateMember(Member member) {
